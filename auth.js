d652f41fd998c3e90e9306770b1e368ea0084242
iodaniel <iodaniel@icloud.com>
2023-01-31 14:22:51 -0700

    OAuth week08

--- auth.js
+++ auth.js
@@ -2,17 +2,21 @@
 // dotenv.config();
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+//const GOOGLE_CLIENT_ID="454712106113-kcq12rtfihc8so8n0j7u0ub3asjlehus.apps.googleusercontent.com"
 const GOOGLE_CLIENT_ID="454712106113-49m8bp3812fnjfvvnlaa0l0sr6tad40g.apps.googleusercontent.com"
+//const GOOGLE_CLIENT_SECRET="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"
 const GOOGLE_CLIENT_SECRET="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"
 passport.use(new GoogleStrategy({
     clientID:GOOGLE_CLIENT_ID,
     clientSecret:GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://food-l5.onrender.com/foods/google/callback",
+    callbackURL: "https://food-l5.onrender.com/google/callback",
+   // "http://localhost:8121/google/callback",
     passReqToCallback: true
   },
+ 
   function(request, accessToken, refreshToken, profile, done) {
     //User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, profile);
+      return done(null, profile);
     //this User.findOrCreate is for the database user.  
   //  });
   }
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const session = require("express-session");
 const passport = require('passport');
 require('../auth');
 //createa route for the contact all ...
+
+function isLoggedIn(req, res, next){
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+}
+  //req.user ? next():res.sendStatus(401);
+  //code 401 is authorized 
+}
+
 router.use('/', require('./swagger'));
+//router.use('/foods', isLoggedIn, require('./foods'));
 router.use('/foods', require('./foods'));
+router.get("/", (req, res) => {
+  res.redirect("/users");
+})
 router.use('/users', require('./users'));
+// router.get('/', (req, res) => {
+//   res.send('Food Datnpm ');
+// });
+// router.get('/', (req, res) => {
+//   res.send('User Data');
+// });
+router.use(session({secret:'tacocat'}));
+
+
 
 router.get('/', (req, res) => {
-  res.send('Food Data');
-});
-router.get('/', (req, res) => {
-  res.send('User Data');
-});
-
-router.get('/users', (req, res) => {
   res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
@@ -24,7 +42,7 @@ router.get('/auth/google',
 
 router.get('/google/callback', 
   passport.authenticate('google',{
-    successRedirect: '/foods', 
+    successRedirect: '/users', 
     failureRedirect: '/auth/failure', 
   })
 );
@@ -34,8 +52,8 @@ router.get('/auth/failure', (req, res) => {
 
 });
 //scope is what type of information we need to request to google. 
-router.get('/protected', (req, res) => {
-  res.send('Hello');
-});
+// router.get('/protected', (req, res) => {
+//   res.send('Hello');
+// });
 
 module.exports = router;
